d4879b7c389b535c94d0c16afec6df3fd881185a
alamin5g <21303134@iubat.edu>
2024-03-05 02:50:27 +0600

    constructor injection with ambiguity problem solution

--- src/main/java/com/goonok/Spring1App/cons_inject/Student.java
+++ src/main/java/com/goonok/Spring1App/cons_inject/Student.java
@@ -1,21 +1,37 @@
 package com.goonok.Spring1App.cons_inject;
 
 public class Student {
- 
+
+	private int id;
 	private String name;
 	private String address;
-	
+
+	public Student(int id, String name, String address) {
+
+		System.out.println("int, st, st - called");
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+
+	public Student(int id, String name) {
+		System.out.println("int, st - called");
+		this.id = id;
+		this.name = name;
+	}
+
 	public Student(String name, String address) {
-		
+		System.out.println("st, st - called");
 		this.name = name;
 		this.address = address;
 	}
 
 	@Override
 	public String toString() {
-		return "Student [name=" + name + ", address=" + address + "]";
+		return "Student [id=" + id + ", name=" + name + ", address=" + address + "]";
 	}
-	
-	
+
+
+
 
 }
