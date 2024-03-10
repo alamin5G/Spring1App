ca40b22c1fc92ee17728ff564f4f63bc7bf1fffd
alamin5g <alaminrakib4864@gmail.com>
2024-03-10 14:20:52 +0600

    jdbc crud-insert,update,deleted,fetch

--- src/main/java/com/goonok/Spring1App/jdbc/Main.java
+++ src/main/java/com/goonok/Spring1App/jdbc/Main.java
@@ -1,10 +1,13 @@
 package com.goonok.Spring1App.jdbc;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class Main {
 
@@ -13,13 +16,59 @@ public class Main {
 		JdbcTemplate jdbc =  context.getBean("jdbcTemplate", JdbcTemplate.class);
 		try {
 			System.out.println(jdbc.getDataSource().getConnection());
-			String sql = "INSERT INTO st_info (id, name,address, phone) VALUES (?, ?, ?, ?)";
-			int rowsAffected = jdbc.update(sql, 1, "Md. Alamin", "Tongi", "01822679672");
-			System.out.println(rowsAffected + " rows affected");
-			
+			/*
+			 * String sql =
+			 * "INSERT INTO st_info (id, name,address, phone) VALUES (?, ?, ?, ?)"; int
+			 * rowsAffected = jdbc.update(sql, 1, "Md. Alamin", "Tongi", "01822679672");
+			 * System.out.println(rowsAffected + " rows affected");
+			 */
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		/*
+		 * sql insert
+		 
+		String insertSql = "insert into st_info(id, name, address, phone)values(?, ?, ?, ?)";
+		int rowAffected = jdbc.update(insertSql, 5, "Rakib", "Uttara, Dhaka", "01581733764");
+		System.out.println("Total " + rowAffected + " row inserted int to the st_info table");
+		*/
+		
+		
+		/*
+		 * sql update
+		 */
+		/*
+		 * String sqlUpdate = "update st_info set name =?, address=? where id = ?"; int
+		 * rowsAffected = jdbc.update(sqlUpdate, "Md. Al-Amin", "Tongi, Gazipur", 1);
+		 * System.out.println("total " + rowsAffected +
+		 * " row data updated on table st_info");
+		 */
+		
+		/*
+		 * sql delete
+		 *
+		 */
+		
+		/*
+		 * String sqlDelete = "delete from st_info where id = ?"; int rowsAffected =
+		 * jdbc.update(sqlDelete, 5); System.out.println(rowsAffected +
+		 * " row deleted succesfully!");
+		 */
+		
+		//fetch or retrive data from the database table
+		
+		String sql = "select * from st_info where id = ?";
+		RowMapper rowMapper = new RowMapper() {
+
+			public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+				String name = rs.getString(2);	//column num
+				return name;
+			}
+		};
+		String name = (String) jdbc.queryForObject(sql, rowMapper, 4); //2 for row number
+		System.out.println(name);
+		
 	}
 }
