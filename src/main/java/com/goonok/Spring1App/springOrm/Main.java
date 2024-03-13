28ca82a600bdb7ceb5f0ce7f2bbbb6f20647f92f
alamin5g <21303134@iubat.edu>
2024-03-13 06:16:22 +0600

    spring done

--- src/main/java/com/goonok/Spring1App/springOrm/Main.java
+++ src/main/java/com/goonok/Spring1App/springOrm/Main.java
@@ -1,5 +1,10 @@
 package com.goonok.Spring1App.springOrm;
 
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
 public class Main {
 
+	public static void main(String[] args) {
+		
+	}
 }
